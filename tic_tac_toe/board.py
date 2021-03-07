8e4d374a3e05175f17f2a940de2552dd8f7b8ffe
Luke-Kurlandski-TCNJ <44705759+Luke-Kurlandski-TCNJ@users.noreply.github.com>
2021-03-07 12:11:52 -0500

    Update board.py

--- tic_tac_toe/board.py
+++ tic_tac_toe/board.py
@@ -133,7 +133,7 @@ class Board:
 		center = self.board[1][1]
 		side_center = [self.board[0][1], self.board[1][0], self.board[2][1], self.board[1][2]]
 
-		x_0, x_1, x_2, x_3, x_4, x_5, x_6 = 0, 0, 0, 0, 0, 0, 0
+		x_0, x_1, x_2, x_3, x_4, x_5, x_6 = 1, 0, 0, 0, 0, 0, 0
 
 		for p in corners:
 			if p == positive_board_piece:
